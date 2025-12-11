016c06118b89f874bfdda05f86b5c275ffdfa779
prajwalpvs <your-email@example.com>
2025-12-10 21:34:08 -0500

    removed env.js file

--- env.js
+++
@@ -1,8 +0,0 @@
-// This file holds your secret API key.
-// IMPORTANT: Do NOT share this file or upload it to a public place like GitHub.
-
-window.process = {
-  env: {
-    API_KEY: "", // <-- PASTE YOUR GEMINI API KEY HERE
-  }
-};
